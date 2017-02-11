b79a1ca33b5bdaf60a0f06ca504f84a590a48c9c
CosmicCandi <kalea.wolff@gmail.com>
2017-02-11 00:36:17 -0500

    Changed the order of the deliverables so they lined up with the actual code that delivers them.

--- chapter_1.rb
+++ chapter_1.rb
@@ -2,20 +2,6 @@
 # Inspired by the book "Head First Ruby: A Brain Friendly Guide"
 # Written without looking at their code examples.
 
-### Deliverables ###
-# Prompt the Player to enter their name
-# Generate a Random Number from 1 to 100
-# Keep track of how many guesses the player has made
-# Before each guess, let them know many guesses (out of 10) they have left
-# Prompt the player to make a guess as to what the target number is
-# If the player's guess is less than the target number say "Oops. Your guess was LOW."
-# If the player's guess is greater than the target number, say "Oops, Your guess was HIGH."
-# If the player's guess is equal to the target number, tell them "Good job, [name]!
-# => You guessed my number in [number of guesses] guesses"
-#If the player runs out of turns without guessing correctly, say, "Sorry, You didn't get my number. My number was [target]."
-#Keep allowing the player to guess until they get it right or run out of turns.
-
-
 class Game
 
   include Comparable
@@ -24,34 +10,46 @@ class Game
 
   def initialize
     @name = name
+    # Generate a Random Number from 1 to 100
     @random_number = rand(1..100)
     @turn_count = 10
+    # Keep track of how many guesses the player has made
     @guesses = 0
 
     new_game
   end
 
   def new_game
-    puts "Please enter your name..."
+    # Prompt the Player to enter their name
+    print "Please enter your name: "
     @name = gets.chomp
     puts "Welcome to the game, #{name}!"
 
+    #Keep allowing the player to guess until they get it right or run out of turns.
     while turn_count > 0
+      # Prompt the player to make a guess as to what the target number is
       puts "Please guess a number from 1 to 100..."
+      # Before each guess, let them know many guesses (out of 10) they have left
       puts "You have #{turn_count} guesses remaining."
       guess = gets.chomp.to_i
       self.guesses += 1
       if guess == random_number
+        # If the player's guess is equal to the target number, tell them "Good job, [name]!
+        # => You guessed my number in [number of guesses] guesses"
         puts "Good job, #{name}, you guessed my number! You guessed my number in #{guesses} guesses."
         play_again?
       elsif guess < random_number
+        # If the player's guess is less than the target number say "Oops. Your guess was LOW."
         puts "Oops. Your guess was LOW."
         self.turn_count -= 1
       elsif guess > random_number
+        # If the player's guess is greater than the target number, say "Oops, Your guess was HIGH."
       puts "Oops. Your guess was HIGH."
         self.turn_count -= 1
       end
   end
+  #If the player runs out of turns without guessing correctly, say, "Sorry,
+  #You didn't get my number. My number was [target]."
   puts "Sorry. You didn't get my number. My number was #{random_number}."
   play_again?
 end
