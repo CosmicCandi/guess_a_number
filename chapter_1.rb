1063ec220c268a4d4838264cb151fdc352d0a8cc
CosmicCandi <kalea.wolff@gmail.com>
2017-02-11 00:27:28 -0500

    Added the deliverables from the book into the comments of the program.

--- chapter_1.rb
+++ chapter_1.rb
@@ -2,6 +2,20 @@
 # Inspired by the book "Head First Ruby: A Brain Friendly Guide"
 # Written without looking at their code examples.
 
+### Deliverables ###
+# Prompt the Player to enter their name
+# Generate a Random Number from 1 to 100
+# Keep track of how many guesses the player has made
+# Before each guess, let them know many guesses (out of 10) they have left
+# Prompt the player to make a guess as to what the target number is
+# If the player's guess is less than the target number say "Oops. Your guess was LOW."
+# If the player's guess is greater than the target number, say "Oops, Your guess was HIGH."
+# If the player's guess is equal to the target number, tell them "Good job, [name]!
+# => You guessed my number in [number of guesses] guesses"
+#If the player runs out of turns without guessing correctly, say, "Sorry, You didn't get my number. My number was [target]."
+#Keep allowing the player to guess until they get it right or run out of turns.
+
+
 class Game
 
   include Comparable
